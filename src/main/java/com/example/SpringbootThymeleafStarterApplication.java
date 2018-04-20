13149f5fa272c7a4df68d08c40b9aa05a7745b18
Qzing <Qzing@DESKTOP-3CBKIOK>
2018-04-20 10:53:14 +0800

    springboot整合thymeleaf

---
+++ src/main/java/com/example/SpringbootThymeleafStarterApplication.java
@@ -0,0 +1,13 @@
+
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootThymeleafStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootThymeleafStarterApplication.class, args);
+	}
+}
